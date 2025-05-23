ca77be50077ac8dd28f531feb9e49a7aed8f3b72
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:14:54 +0100

    Removed exercises/4169e0d6-47f4-441f-aad1-ea96c36f0f3c/solutions/c2cf6af6-effe-41a0-8329-8d5a73475baa/sol.py

--- exercises/4169e0d6-47f4-441f-aad1-ea96c36f0f3c/solutions/c2cf6af6-effe-41a0-8329-8d5a73475baa/sol.py
+++
@@ -1,9 +0,0 @@
-bitcoin_price = 39500  # Change this value for testing
-
-# Check price conditions
-if bitcoin_price < 40000:
-    print("Warning: Bitcoin price dropped below $40,000!")
-elif bitcoin_price > 50000:
-    print("Alert: Bitcoin price rose above $50,000!")
-else:
-    print("Bitcoin price is stable.")
