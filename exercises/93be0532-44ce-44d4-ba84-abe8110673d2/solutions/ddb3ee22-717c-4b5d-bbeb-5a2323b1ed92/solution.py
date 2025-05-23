f2be9be25f17d057ace06e9b1407e6ccc9b4478c
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:11:18 +0100

    Removed exercises/93be0532-44ce-44d4-ba84-abe8110673d2/solutions/ddb3ee22-717c-4b5d-bbeb-5a2323b1ed92/solution.py

--- exercises/93be0532-44ce-44d4-ba84-abe8110673d2/solutions/ddb3ee22-717c-4b5d-bbeb-5a2323b1ed92/solution.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt sources = ['Solar', 'Wind', 'Hydro', 'Other Renewables']
-percentages = [35, 28, 22, 15]
-explode = (0.1, 0.05, 0, 0)
-colors = ['#f1c40f', '#3498db', '#2ecc71', '#95a5a6'] plt.figure(figsize=(10, 8))
-plt.pie(percentages, explode=explode, labels=sources, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
-plt.title('Global Renewable Energy Mix')
-plt.axis('equal')
-plt.show()
