42a00e0cc1a4118cdfcc2cdcdede3fe90d25dd2b
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:13:11 +0100

    Removed exercises/d2ef3a48-bf3c-41d3-bab4-109d3d01a36a/solutions/76ed4113-2ff3-4224-b66c-0b62024909c7/solution.py

--- exercises/d2ef3a48-bf3c-41d3-bab4-109d3d01a36a/solutions/76ed4113-2ff3-4224-b66c-0b62024909c7/solution.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd data = { 'Coin': ['BTC', 'ETH', 'DOT'], 'Price': [45000, 3000, 30], 'Market_Cap': [850000000000, 350000000000, 30000000000], '24h_Change': [2.3, -1.2, 5.4]
-} df = pd.DataFrame(data) print("Coins with positive change:")
-print(df[df['24h_Change'] > 0]) print("\nHigh Market Cap coins:")
-print(df[df['Market_Cap'] > 100000000000]) print("\nSorted by Price:")
-print(df.sort_values('Price', ascending=False)) df['Status'] = df['Market_Cap'].apply(lambda x: 'High Cap' if x > 500000000000 else 'Regular')
-print("\nWith Status:")
-print(df)
