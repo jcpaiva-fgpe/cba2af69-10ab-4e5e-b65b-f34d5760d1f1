279f422b0119afdaa600e87578bd1a78d15574d5
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:10:04 +0100

    Removed exercises/74813b4b-6c0d-454e-85b3-9d85176839f0/solutions/1f5cb66d-2873-4e81-8250-eee5e4c78dca/solution.py

--- exercises/74813b4b-6c0d-454e-85b3-9d85176839f0/solutions/1f5cb66d-2873-4e81-8250-eee5e4c78dca/solution.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd data = { 'Exchange': ['Binance', 'Binance', 'Coinbase', 'Coinbase'], 'Coin': ['BTC', 'ETH', 'BTC', 'ETH'], 'Volume': [1000, 5000, 800, 4000], 'Avg_Price': [45000, 3000, 45100, 3010]
-} df = pd.DataFrame(data) print("Total volume per exchange:")
-print(df.groupby('Exchange')['Volume'].sum()) print("\nAverage price per coin:")
-print(df.groupby('Coin')['Avg_Price'].mean()) print("\nTrading pairs per exchange:")
-print(df.groupby('Exchange').size())
