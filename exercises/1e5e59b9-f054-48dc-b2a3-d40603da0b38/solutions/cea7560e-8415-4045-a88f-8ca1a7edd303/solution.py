f11862f296f319123b5e5d422d4efb7c85ef40dc
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:10:57 +0100

    Removed exercises/1e5e59b9-f054-48dc-b2a3-d40603da0b38/solutions/cea7560e-8415-4045-a88f-8ca1a7edd303/solution.py

--- exercises/1e5e59b9-f054-48dc-b2a3-d40603da0b38/solutions/cea7560e-8415-4045-a88f-8ca1a7edd303/solution.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt coins = ['Bitcoin', 'Ethereum', 'Binance Coin', 'Others']
-market_share = [42, 20, 15, 23]
-explode = (0.1, 0.05, 0, 0)
-colors = ['#f1c40f', '#2ecc71', '#3498db', '#95a5a6'] plt.figure(figsize=(10, 8))
-plt.pie(market_share, explode=explode, labels=coins, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
-plt.title('Crypto Market Dominance')
-plt.axis('equal')
-plt.show()
