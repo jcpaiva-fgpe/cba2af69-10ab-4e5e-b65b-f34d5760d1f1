d0545edf0225757e287a81f4c2e497c793a6cf20
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:14:15 +0100

    Removed exercises/7f79346e-c318-4430-b81a-cc48d030e693/solutions/2e0d6349-587d-43a4-9abc-2fd1347c0dcb/solution.py

--- exercises/7f79346e-c318-4430-b81a-cc48d030e693/solutions/2e0d6349-587d-43a4-9abc-2fd1347c0dcb/solution.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd data = { 'Zone': ['Tropical', 'Tropical', 'Temperate', 'Temperate'], 'Region': ['Amazon', 'Congo', 'Europe', 'North America'], 'Temperature': [28, 27, 15, 12], 'Rainfall': [2500, 1800, 700, 850]
-} df = pd.DataFrame(data) print("Average temperature per zone:")
-print(df.groupby('Zone')['Temperature'].mean()) print("\nTotal rainfall per zone:")
-print(df.groupby('Zone')['Rainfall'].sum()) print("\nRegions per zone:")
-print(df.groupby('Zone').size())
