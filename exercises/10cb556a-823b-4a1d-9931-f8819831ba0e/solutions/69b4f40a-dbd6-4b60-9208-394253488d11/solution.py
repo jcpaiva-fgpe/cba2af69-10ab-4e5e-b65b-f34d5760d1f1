c5b2611da7fd3f10f2959e0f1853acd4fd2be819
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:10:32 +0100

    Removed exercises/10cb556a-823b-4a1d-9931-f8819831ba0e/solutions/69b4f40a-dbd6-4b60-9208-394253488d11/solution.py

--- exercises/10cb556a-823b-4a1d-9931-f8819831ba0e/solutions/69b4f40a-dbd6-4b60-9208-394253488d11/solution.py
+++
@@ -1,2 +0,0 @@
-temperatures = [32.5, 33.1, 33.8, 33.4, 33.9]
-for i in range(len(temperatures)-1): if temperatures[i+1] > temperatures[i]: print("Temperature Increased") else: print("Temperature Decreased")
