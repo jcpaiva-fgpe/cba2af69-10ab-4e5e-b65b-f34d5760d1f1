ddeac7274d6155e576c5c5e5ee4defb221b403a1
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:09:25 +0100

    Removed exercises/a302f750-5c12-4bfe-b2f9-dbd8c625c483/solutions/055d60d9-778d-4dd8-9762-378ef1eb452b/solution.py

--- exercises/a302f750-5c12-4bfe-b2f9-dbd8c625c483/solutions/055d60d9-778d-4dd8-9762-378ef1eb452b/solution.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd data = { 'Region': ['Arctic', 'Amazon', 'Mediterranean'], 'Temperature': [-5, 28, 22], 'Sea_Level_Rise': [3.2, 0, 2.8], 'CO2_Level': [412, 405, 415]
-} df = pd.DataFrame(data)
-print("Global Climate Indicators:")
-print(df)
-print("\nBasic Statistics:")
-print(df.describe())
