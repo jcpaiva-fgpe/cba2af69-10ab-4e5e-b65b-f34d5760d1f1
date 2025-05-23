e70bb4a6b9858b8350c373015d15bb5ab1e3861b
Agni Agni <y4nm45@gmail.com>
2025-05-23 08:11:36 +0100

    Removed exercises/176ba9fe-d9ec-4714-a0b0-a3a763c94a82/solutions/629074e1-fa7a-4d0b-803c-5bfa04c97691/solution.py

--- exercises/176ba9fe-d9ec-4714-a0b0-a3a763c94a82/solutions/629074e1-fa7a-4d0b-803c-5bfa04c97691/solution.py
+++
@@ -1,4 +0,0 @@
-target_price = 45000
-current_price = 46500 if current_price > target_price: print("Price above target")
-elif current_price < target_price: print("Price below target")
-else: print("Price at target") if abs(current_price - target_price) >= 1000: print("Price near target")
